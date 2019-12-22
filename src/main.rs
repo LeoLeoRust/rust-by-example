a97da36f0a5aa1680fb84f01c23ca4aba3af1b91
leoleogo <dcj1103@gmail.com>
2019-12-22 22:53:37 +0800

    add ch3

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,16 @@ pub mod ch2 {
     pub mod primitives;
 }
 
+pub mod ch3 {
+    pub mod custom_types;
+}
+
 fn main() {
     // println!("Hello, world!");
     // ch1::hello_world::display();
     // ch1::hello_world::format();
     // ch2::primitives::literals();
     // ch2::primitives::tuples();
-    ch2::primitives::array_slice();
+    // ch2::primitives::array_slice();
+    ch3::custom_types::struc();
 }
